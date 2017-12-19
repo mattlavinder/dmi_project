2fa0d5e570f0b4961b7349a2297b55ecada1cfea
Chase Counsell <cjc0028@auburn.edu>
2017-12-19 17:58:00 -0600

    Load in categories and products from db.

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -2,17 +2,66 @@
 
 class Order extends CI_Controller
 {
-    public function start()
+    public function __construct()
     {
+        parent::__construct();
+        $this->load->model('items_model');
+        $this->load->model('order_model');
+        $this->load->model('categories_model');
+//        $this->load->model('order_items_model');
+    }
+
+    public function shop($order_id)
+    {
+        // TODO Check if order is completed
+
+
+        // Check if category has been set
+        if (null === $this->input->get('category'))
+        {
+            // If not get all items
+            $data['items'] = $this->items_model->get_items();
+        }
+        else
+        {
+            // Else get items in selected category
+            $data['items'] = $this->items_model->get_items($this->input->get('category'));
+        }
+
+        $data['categories'] = $this->categories_model->get_categories();
+        $data['order_id'] = $order_id;
+
         $this->load->view('includes/header_view');
-        $this->load->view('order/start_view');
+        $this->load->view('order/add_items_view', $data);
         $this->load->view('includes/footer_view');
     }
 
-    public function shop()
+    public function new_order()
     {
-        $this->load->view('includes/header_view');
-        $this->load->view('order/add_items_view');
-        $this->load->view('includes/footer_view');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        // Validate form
+        $this->form_validation->set_rules('phone_number', 'Phone Number', 'required|min_length[10]|max_length[12]|numeric');
+        $this->form_validation->set_rules('pickup_time', 'Pickup Time', 'required');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->view('includes/header_view');
+            $this->load->view('order/new_order_view');
+            $this->load->view('includes/footer_view');
+        }
+        else
+        {
+            $phone_number = $this->input->post('phone_number');
+            $pickup_time = $this->input->post('pickup_time');
+            $order_id = $this->order_model->create_order($phone_number, $pickup_time);
+            redirect('order/shop/'.$order_id);
+        }
+    }
+
+    public function order_complete()
+    {
+
     }
 }
