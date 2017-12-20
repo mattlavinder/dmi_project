569425716e4ef2b45ffce492633e3896b3e1ff18
Chase Counsell <cjc0028@auburn.edu>
2017-12-19 18:03:50 -0600

    Displayed categories and products.

--- application/views/order/add_items_view.php
+++ application/views/order/add_items_view.php
@@ -1,34 +1,43 @@
-<?php
-    $phone_number = $_GET['phone_number'];
-    $pickup_time = $_GET['pickup_time'];
-    ?>
-
     <div class="row">
         <div class="col-lg-2">
-<!--            <h1 class="my-4">Martha's Brew Stand</h1>-->
+            <h4 class="my-4">Categories</h4>
             <div class="list-group" id="categories">
-                <a href="#" class="list-group-item active">All</a>
-                <a href="#" class="list-group-item">Category 2</a>
-                <a href="#" class="list-group-item">Category 3</a>
+                <a href="<?=base_url()?>order/shop/<?=$order_id?>" class="list-group-item active">All</a>
+                <?php foreach ($categories as $category): ?>
+                <a href="<?=base_url()?>order/shop/<?=$order_id?>?category=<?=$category['id']?>" class="list-group-item"><?=$category['name']?></a>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="col-lg-8">
             <div class="row">
+                <?php foreach ($items as $item): ?>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
                         <a href="#"><img class="card-img-top" src="http://placehold.it/700x700" alt=""></a>
                         <div class="card-body">
                             <h4 class="card-title">
-                                <a href="#">Item One</a>
+                                <p><?=$item['name']?></p>
                             </h4>
-                            <h5>$24.99</h5>
+                            <h5>$<?=$item['cost']?></h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
                         </div>
                     </div>
                 </div>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="col-lg-2">
-            <h4 class="h4">Cart</h4>
+            <h4 class="my-h4">Cart</h4>
+            <div class="">
+                <table class="table table-striped table-hover table-bordered table-sm">
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
