48c3b00f62bb84f8f1ab5d956a8175162ab71975
Matt Lavinder <mlavinder@data-management.com>
2017-12-20 15:29:06 -0500

    Update coffee_shop_item_categories.sql

--- sql/coffee_shop_item_categories.sql
+++ sql/coffee_shop_item_categories.sql
@@ -2,7 +2,7 @@ CREATE TABLE coffee_shop.item_categories
 (
     id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name varchar(16) NOT NULL,
-    created_at timestamp NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO coffee_shop.item_categories (id, name, created_at) VALUES (1, 'Hot', '2017-12-12 22:03:40');
-INSERT INTO coffee_shop.item_categories (id, name, created_at) VALUES (2, 'Cold', '2017-12-12 22:05:30');
+INSERT INTO coffee_shop.item_categories (name) VALUES ('Hot');
+INSERT INTO coffee_shop.item_categories (name) VALUES ('Cold');
